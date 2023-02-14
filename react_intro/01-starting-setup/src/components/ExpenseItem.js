5e7d5e6a425609a738da798fc059281a98a72b8e
Bryson Jones <bkjones97@gmail.com>
2023-02-13 17:22:59 -0800

    test adding in state hooks in react

--- react_intro/01-starting-setup/src/components/ExpenseItem.js
+++ react_intro/01-starting-setup/src/components/ExpenseItem.js
@@ -1,18 +1,30 @@
+
+import React, {useState } from 'react'
 import './ExpenseItem.css';
 
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
+
+    const [title, setTitle] = useState(props.title);
+
+    function clickHandler()
+    {
+        setTitle('Updated!');
+    }
   
     return (
       <div className="expense-item">
         <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
-          <h2>{props.title}</h2>
+          <h2>{title}</h2>
           <div className="expense-item__price">{props.amount}</div>
+          <button onClick={clickHandler}>Change title</button>
         </div>
       </div>
     );
   }
   
   export default ExpenseItem;
+
+  

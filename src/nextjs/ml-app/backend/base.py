bc5334c6f993199d2ac9c92b52578463782efbec
Bryson Jones <bkjones97@gmail.com>
2023-03-23 21:31:47 -0700

    remove dead code

--- src/nextjs/ml-app/backend/base.py
+++ src/nextjs/ml-app/backend/base.py
@@ -75,14 +75,5 @@ def upload_image():
                         image=data
                         ), 200
 
-@socketio.on('connect')
-def connect():
-    print('Client connecting...')
-
-@socketio.on('message')
-def handle_message(data):
-    print('received message: ' + data)
-
-
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5000)
